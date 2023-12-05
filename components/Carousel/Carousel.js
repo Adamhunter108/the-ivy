f1b13b7d724188252adbef76dd39d295d4e9f200
Adamhunter108 <61752774+Adamhunter108@users.noreply.github.com>
2023-12-05 15:31:49 -0500

    Carousel component clean up and image update

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -6,14 +6,42 @@ import Image from "next/image";
 const images = [
   {
     id: 1,
-    src: "/images/carousel/insideshop1.jpg",
+    src: "/images/carousel/1.jpg",
   },
   {
     id: 2,
-    src: "/images/carousel/insideshop2.jpg",
+    src: "/images/carousel/2.jpg",
   },
   {
     id: 3,
+    src: "/images/carousel/3.jpg",
+  },
+  {
+    id: 4,
+    src: "/images/carousel/4.jpg",
+  },
+  {
+    id: 5,
+    src: "/images/carousel/5.jpg",
+  },
+  {
+    id: 6,
+    src: "/images/carousel/6.jpg",
+  },
+  {
+    id: 7,
+    src: "/images/carousel/7.jpg",
+  },
+  {
+    id: 8,
+    src: "/images/carousel/insideshop1.jpg",
+  },
+  {
+    id: 9,
+    src: "/images/carousel/insideshop2.jpg",
+  },
+  {
+    id: 10,
     src: "/images/carousel/insideshop3.jpg",
   },
 ];
@@ -31,105 +59,21 @@ export const EmblaCarousel = () => {
   return (
     <div className="embla mx-auto max-w-screen-lg w-full" ref={emblaRef}>
       <div className="embla__container">
-        <div className="embla__slide">
-          <Image
-            className="w-full h-auto rounded-xl"
-            src="/images/carousel/insideshop1.jpg"
-            height={650}
-            width={1000}
-            alt="Image 1"
-            blurDataURL="/images/carousel/insideshop1.jpg"
-            placeholder="blur"
-            loading="lazy"
-          />
-        </div>
-        <div className="embla__slide">
-          <Image
-            className="w-full h-auto rounded-xl"
-            src="/images/carousel/insideshop2.jpg"
-            height={650}
-            width={1000}
-            alt="Image 2"
-            blurDataURL="/images/carousel/insideshop2.jpg"
-            placeholder="blur"
-            loading="lazy"
-          />
-        </div>
-        <div className="embla__slide">
-          <Image
-            className="w-full h-auto rounded-xl"
-            src="/images/carousel/insideshop3.jpg"
-            height={650}
-            width={1000}
-            alt="Image 3"
-            blurDataURL="/images/carousel/insideshop3.jpg"
-            placeholder="blur"
-            loading="lazy"
-          />
-        </div>
+        {images.map((image) => (
+          <div key={image.id} className="embla__slide">
+            <Image
+              className="w-full h-auto rounded-xl"
+              src={`${image.src}`}
+              height={650}
+              width={1000}
+              alt={`Image ${image.id}`}
+              blurDataURL={`${image.src}`}
+              placeholder="blur"
+              loading="lazy"
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
-
-  // 👇 Started prev & next arrows
-  // return (
-  //     <div className="embla">
-  //         <div className="embla__container" ref={emblaRef}>
-
-  //             <div className="embla__slide">
-  //                 <Image
-  //                 className="max-w-full h-auto rounded-xl"
-  //                 src="/images/carousel/insideshop1.jpg"
-  //                 height={550}
-  //                 width={850}
-  //                 alt="Flyer 1"
-  //                 blurDataURL="/images/carousel/insideshop1.jpg"
-  //                 placeholder="blur"
-  //                 loading="lazy"
-  //                 />
-  //             </div>
-  //             <div className="embla__slide">
-  //                 <Image
-  //                 className="max-w-full h-auto rounded-xl"
-  //                 src="/images/carousel/insideshop2.jpg"
-  //                 height={550}
-  //                 width={850}
-  //                 alt="Flyer 1"
-  //                 blurDataURL="/images/carousel/insideshop2.jpg"
-  //                 placeholder="blur"
-  //                 loading="lazy"
-  //                 />
-  //             </div>
-  //             <div className="embla__slide">
-  //                 <Image
-  //                 className="max-w-full h-auto rounded-xl"
-  //                 src="/images/carousel/insideshop3.jpg"
-  //                 height={550}
-  //                 width={850}
-  //                 alt="Flyer 1"
-  //                 blurDataURL="/images/carousel/insideshop3.jpg"
-  //                 placeholder="blur"
-  //                 loading="lazy"
-  //                 />
-  //             </div>
-
-  //         </div>
-  //         <button
-  //             className={`embla__button embla__button--prev ${prevBtnEnabled ? "" : "is-disabled"}`}
-  //             onClick={scrollPrev}
-  //             disabled={!prevBtnEnabled}
-  //             aria-label="Previous slide"
-  //         >
-  //             <PrevArrow />
-  //         </button>
-  //         <button
-  //             className={`embla__button embla__button--next ${nextBtnEnabled ? "" : "is-disabled"}`}
-  //             onClick={scrollNext}
-  //             disabled={!nextBtnEnabled}
-  //             aria-label="Next slide"
-  //         >
-  //             <NextArrow />
-  //         </button>
-  //     </div>
-  // );
 };
