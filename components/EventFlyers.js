43209a8369f87f0c35c8d04f9f391c977113481b
Adamhunter108 <61752774+Adamhunter108@users.noreply.github.com>
2024-07-03 13:02:06 -0400

    Flyer update & clean-up

--- components/EventFlyers.js
+++ components/EventFlyers.js
@@ -29,6 +29,52 @@ export default function EventFlyers() {
           <div>
             <div className="flex justify-center mb-6">
               <a
+                href="https://square.link/u/hRsWypTy"
+                target="_blank"
+                rel="noreferrer"
+                className="flex flex-row"
+              >
+                <button
+                  type="button"
+                  className="z-30 px-3.5 mt-8 py-2 bg-gradient-to-b from-neon-pink via-neon-pink to-pink-400 text-white hover:bg-gradient-to-t from-neon-pink via-neon-pink to-pink-400 hover:text-white font-light rounded-md text-lg flex flex-row items-center"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-8 h-8"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 010 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 010-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375z"
+                    />
+                  </svg>
+                  <div className="pl-3">Sip & Paint @ Bright Eye Beer Co.</div>
+                </button>
+              </a>
+            </div>
+            <div className="hover:scale-110 ease-in-out duration-200">
+              <Image
+                data-aos="fade-up"
+                data-aos-delay="200"
+                className="max-w-full h-auto rounded-xl"
+                src="/images/flyer-display/7-11-24.jpg"
+                height={450}
+                width={450}
+                alt="Flyer 1"
+                blurDataURL="/images/flyer-display/7-11-24.jpg"
+                placeholder="blur"
+                loading="lazy"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex justify-center mb-6">
+              <a
                 href="https://square.link/u/v7PfEDBx"
                 target="_blank"
                 rel="noreferrer"
@@ -52,53 +98,7 @@ export default function EventFlyers() {
                       d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 010 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 010-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375z"
                     />
                   </svg>
-                  <div className="pl-3">LB Cleanup Sip & Paint</div>
-                </button>
-              </a>
-            </div>
-            <div className="hover:scale-110 ease-in-out duration-200">
-              <Image
-                data-aos="fade-up"
-                data-aos-delay="200"
-                className="max-w-full h-auto rounded-xl"
-                src="/images/flyer-display/5-23-24.jpg"
-                height={450}
-                width={450}
-                alt="Flyer 1"
-                blurDataURL="/images/flyer-display/5-23-24.jpg"
-                placeholder="blur"
-                loading="lazy"
-              />
-            </div>
-          </div>
-
-          <div>
-            <div className="flex justify-center mb-6">
-              <a
-                href="https://square.link/u/IrMIreNi"
-                target="_blank"
-                rel="noreferrer"
-                className="flex flex-row"
-              >
-                <button
-                  type="button"
-                  className="z-30 px-3.5 mt-8 py-2 bg-gradient-to-b from-neon-pink via-neon-pink to-pink-400 text-white hover:bg-gradient-to-t from-neon-pink via-neon-pink to-pink-400 hover:text-white font-light rounded-md text-lg flex flex-row items-center"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="w-8 h-8"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 010 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 010-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375z"
-                    />
-                  </svg>
-                  <div className="pl-3">Taylor Swift Craft Night</div>
+                  <div className="pl-3">Sip & Paint: Shell Painting</div>
                 </button>
               </a>
             </div>
@@ -108,11 +108,11 @@ export default function EventFlyers() {
                 data-aos="fade-up"
                 data-aos-delay="300"
                 className="max-w-full h-auto rounded-xl"
-                src="/images/flyer-display/6-5-24.jpg"
+                src="/images/flyer-display/7-25-24.jpg"
                 height={450}
-                width={350}
+                width={450}
                 alt="Flyer 2"
-                blurDataURL="/images/flyer-display/6-5-24.jpg"
+                blurDataURL="/images/flyer-display/7-25-24.jpg"
                 placeholder="blur"
                 loading="lazy"
               />
